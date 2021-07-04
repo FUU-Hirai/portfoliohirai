ed2db27ee2d1742212fcefb3c955687ee51f276d
FUU-Hirai <83876507+FUU-Hirai@users.noreply.github.com>
2021-07-04 13:32:46 +0900

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -50,8 +50,7 @@ $routes->scope('/', function (RouteBuilder $builder) {
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, templates/Pages/home.php)...
      */
-//     $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
-    $builder->connect('/', ['controller' => 'main', 'action' => 'index']);
+    $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
 
 
     /*
